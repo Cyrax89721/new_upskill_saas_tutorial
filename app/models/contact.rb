7136936b1af1b05a71eee41709edb23b286c48d2
Tyler Polzin <Cyrax89721@hotmail.com>
2016-12-02 01:33:24 +0000

    Added code comments

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < ActiveRecord::Base
+    # Contact form valications
     validates :name, presence: true
     validates :email, presence: true
     validates :comments, presence: true
